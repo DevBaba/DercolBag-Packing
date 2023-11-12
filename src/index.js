5ed92984ba688aba7d5b2c79dcd754b9cfa8e4a7
Ransford Yeboah <132473175+DevBaba@users.noreply.github.com>
2023-11-12 23:27:47 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { Link, animateScroll as scroll } from "react-scroll";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
